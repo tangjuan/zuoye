6c219fb0f7c90b2a046c6f4fe94a0d498b6365c8
PhonebeL <phoebeliaoyf@gmail.com>
2014-01-11 08:15:54 +0800

    3

--- src/com/mydouban/douban/GroupActivity.java
+++ src/com/mydouban/douban/GroupActivity.java
@@ -1,4 +1,4 @@
-package com.chenyc.douban;
+﻿package com.chenyc.douban;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +11,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
+import android.widget.AdapterView; 
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
